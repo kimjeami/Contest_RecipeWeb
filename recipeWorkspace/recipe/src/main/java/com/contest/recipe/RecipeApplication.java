28f8b481cfd58592193c3ca934bb4ad4fbd16d18
DESKTOP-KK5CH74\Administrator <>
2024-08-19 12:49:55 +0900

    기본

---
+++ recipeWorkspace/recipe/src/main/java/com/contest/recipe/RecipeApplication.java
@@ -0,0 +1,13 @@
+package com.contest.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeApplication.class, args);
+	}
+
+}
