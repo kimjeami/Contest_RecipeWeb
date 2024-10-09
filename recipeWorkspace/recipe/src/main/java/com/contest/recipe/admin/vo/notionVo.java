38e238f08b5d3e6f5729f0cc3899c4de7c7bb2a0
mspark303 <mspark303@gmail.com>
2024-10-09 11:09:38 +0900

    관리자만

---
+++ recipeWorkspace/recipe/src/main/java/com/contest/recipe/admin/vo/notionVo.java
@@ -0,0 +1,19 @@
+package com.contest.recipe.admin.vo;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 공지사항 데이터 VO
+ */
+@Data
+public class notionVo {
+    private Integer no;
+    private String title;
+    private String content;
+    private Integer hit;
+    private LocalDateTime enrollDate;
+    private LocalDateTime modifyDate;
+    private Boolean delYn;
+}
