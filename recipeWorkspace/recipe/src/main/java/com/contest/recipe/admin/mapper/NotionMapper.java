7cca8c6f18e956a2771797ba4e661073b332b165
kimjeami <solomon0213@gmail.com>
2024-10-19 13:55:52 +0900

    공지수정

--- recipeWorkspace/recipe/src/main/java/com/contest/recipe/admin/mapper/NotionMapper.java
+++ recipeWorkspace/recipe/src/main/java/com/contest/recipe/admin/mapper/NotionMapper.java
@@ -4,6 +4,7 @@ package com.contest.recipe.admin.mapper;
 import com.contest.recipe.admin.vo.notionVo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -18,9 +19,15 @@ public interface NotionMapper {
 
 
     @Select("""
-            SELECT TITLE,CONTENT,HIT,TO_CHAR(ENROLL_DATE,'YYYY.MM.DD') as enrollDate, TO_CHAR(MODIFY_DATE,'YYYY.MM.DD') as modifyDate, DEL_YN as delYn
+            SELECT NO, TITLE,CONTENT,HIT,TO_CHAR(ENROLL_DATE,'YYYY.MM.DD') as enrollDate, TO_CHAR(MODIFY_DATE,'YYYY.MM.DD') as modifyDate, DEL_YN as delYn
             FROM NOTION
             WHERE no = #{no}
             """)
     notionVo notionDetailData(notionVo vo);
+
+
+    @Update("""
+            UPDATE NOTION SET TITLE=#{title}, CONTENT = #{content}, MODIFY_DATE = CURRENT_TIMESTAMP WHERE NO = #{no}
+            """)
+    int notionModify(notionVo vo);
 }
