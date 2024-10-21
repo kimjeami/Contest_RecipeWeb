4cfd4d6c35088473dc8f6ab260feec8813cc6b03
mspark303 <mspark303@gmail.com>
2024-10-21 11:25:41 +0900

    어드민 홈, QNA

--- recipeWorkspace/recipe/src/main/java/com/contest/recipe/admin/dao/QnaRequestDao.java
+++ recipeWorkspace/recipe/src/main/java/com/contest/recipe/admin/dao/QnaRequestDao.java
@@ -5,6 +5,9 @@ import com.contest.recipe.admin.vo.QnaRequestVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * QNA_REQUEST DAO
  */
@@ -14,6 +17,7 @@ public class QnaRequestDao {
 
     private final QnaRequestMapper qnaRequestMapper;
 
+    // 기본 CRUD
     public Boolean insert_rowAll(QnaRequestVo vo) { return qnaRequestMapper.insert_rowAll(vo); }
 
     public QnaRequestVo select_rowSpecific_all(Integer qnaRequestNo) { return qnaRequestMapper.select_rowSpecific_all(qnaRequestNo); }
@@ -21,5 +25,11 @@ public class QnaRequestDao {
     public Integer update_rowSpecific_all(QnaRequestVo vo) { return qnaRequestMapper.update_rowSpecific_all(vo); }
 
 //    public
+
+    /**
+     * 관리자 메인페이지 출력용
+     * @return
+     */
+    public List<HashMap<String, Object>> select_recentTen() { return qnaRequestMapper.select_recentTen(); }
 
 }
